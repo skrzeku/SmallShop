362d1cc0befd56870317084dbf4dce7fd6ff1cc3
skrzeku <pawelskrzeszewski92@gmail.com>
2018-02-26 20:00:16 +0100

    small styles changes

--- src/app/core-module/core-module.module.ts
+++ src/app/core-module/core-module.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NaviComponent } from './navi/navi.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [NaviComponent],
   declarations: [NaviComponent]
